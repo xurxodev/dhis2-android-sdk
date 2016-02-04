60889f5432d12b10d0cc2312405c982a4611ea77
idelcano <ignaciodelcano@gmail.com>
2016-02-04 17:35:39 +0100

    Added deserialize OrganisationUnit properties

--- app/src/main/java/org/hisp/dhis/android/sdk/controllers/metadata/MetaDataController.java
+++ app/src/main/java/org/hisp/dhis/android/sdk/controllers/metadata/MetaDataController.java
@@ -666,11 +666,21 @@ public final class MetaDataController extends ResourceController {
         }
             List<DbOperation> operations = AssignedProgramsWrapper.getOperations(organisationUnits);
             for(OrganisationUnit organisationUnit:organisationUnits){
-                final Map<String, String> QUERY_MAP_FULL = new HashMap<>();
-                QUERY_MAP_FULL.put("fields","attributeValues[*,attribute[name,displayName,created,lastUpdated,access,id,valueType,code]]");
                 List<OrganisationUnitAttributeValue> organisationUnitAttributeValues = null;
                 try {
-                    organisationUnitAttributeValues=AssignedProgramsWrapper.deserializeAttributeValues(dhisApi.getOrganistationUnit(organisationUnit.getId(), QUERY_MAP_FULL), organisationUnit);
+                    Map<String, String> QUERY_MAP_FULL = new HashMap<>();
+                    QUERY_MAP_FULL.put("fields","[:all],!parent");
+                    response= dhisApi.getOrganistationUnit(organisationUnit.getId(), QUERY_MAP_FULL);
+                    organisationUnit=AssignedProgramsWrapper.deserializeOrganisationUnit(response, organisationUnit);
+                    if(organisationUnit.getDataSets()!=null)
+                    operations.addAll(AssignedProgramsWrapper.saveDataSets(organisationUnit));
+                    if(organisationUnit.getOrganisationUnitGroups()!=null)
+                    operations.addAll(AssignedProgramsWrapper.saveOrganisationUnitGroups(organisationUnit));
+
+                    QUERY_MAP_FULL = new HashMap<>();
+                    QUERY_MAP_FULL.put("fields","*,attributeValues[*,attribute[name,displayName,created,lastUpdated,access,id,valueType,code]],all");
+                    response= dhisApi.getOrganistationUnit(organisationUnit.getId(), QUERY_MAP_FULL);
+                    organisationUnitAttributeValues = AssignedProgramsWrapper.deserializeAttributeValues(response, organisationUnit);
 
                     for(OrganisationUnitAttributeValue organisationUnitAttributeValue:organisationUnitAttributeValues)
                     {
