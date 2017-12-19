8e4bc6fcbca383bfa5d214e1d5a1a14e2055d59b
mplazaspalacio <manuelplazaspalacio@gmail.com>
2017-12-19 23:13:14 +0100

    Modify build.sh and .travis.yml  for take into account new test sizes. (@SmallTest, @MediumTest, @LargeTest)

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -xe
+size='small'
+
+while getopts ":s:" opt; do
+    case $opt in
+    s) size="${OPTARG}"
+    ;;
+    esac
+done
+
 
 # You can run it from any directory.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -8,4 +17,9 @@ PROJECT_DIR=$DIR/
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
 "$PROJECT_DIR"/gradlew --no-daemon clean
 "$PROJECT_DIR"/gradlew --no-daemon build -Dscan
-"$PROJECT_DIR"/gradlew --no-daemon connectedAndroidTest
+"$PROJECT_DIR"/gradlew --no-daemon test
+"$PROJECT_DIR"/gradlew --no-daemon connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=small
+if [ "$size" == "medium" ] || [ "$size" == "large" ];
+then "$PROJECT_DIR"/gradlew --no-daemon connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=medium; fi
+if [ "$size" == "large" ];
+then "$PROJECT_DIR"/gradlew --no-daemon connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=large; fi
