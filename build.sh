c434eeba986e1858ea0731153a703770742036ff
mplazaspalacio <manuelplazaspalacio@gmail.com>
2017-12-21 12:35:17 +0100

    Paralleling travis tests.

--- build.sh
+++ build.sh
@@ -6,9 +6,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_DIR=$DIR/
 
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
-"$PROJECT_DIR"/gradlew --no-daemon clean build
-"$PROJECT_DIR"/gradlew --no-daemon build
+if [ "$TEST_SUITE" == "units" ];
+then
+"$PROJECT_DIR"/gradlew --no-daemon clean
+"$PROJECT_DIR"/gradlew --no-daemon build -Dscan
+"$PROJECT_DIR"/gradlew --no-daemon test
+"$PROJECT_DIR"/gradlew --no-daemon connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=small; fi
+if [ "$TEST_SUITE" == "integration" ];
+then
 if [ "$TRAVIS_BRANCH" == "development" ] || [ "$TRAVIS_BRANCH" == "master" ];
 then "$PROJECT_DIR"/gradlew --no-daemon connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=medium; fi
 if [ "$TRAVIS_BRANCH" == "master" ];
-then "$PROJECT_DIR"/gradlew --no-daemon connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=large; fi
+then "$PROJECT_DIR"/gradlew --no-daemon connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=large; fi;
+fi
