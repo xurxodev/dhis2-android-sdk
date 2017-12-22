d66c490ed03f3d2d7f0b50083c5ab0ec7db2e277
mplazaspalacio <manuelplazaspalacio@gmail.com>
2017-12-22 10:31:05 +0100

    Removing no daemon in build.sh

--- build.sh
+++ build.sh
@@ -8,16 +8,16 @@ PROJECT_DIR=$DIR/
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
 if [ "$TEST_SUITE" == "units" ];
 then
-"$PROJECT_DIR"/gradlew --no-daemon clean
-"$PROJECT_DIR"/gradlew --no-daemon build -Dscan
-"$PROJECT_DIR"/gradlew --no-daemon test
-"$PROJECT_DIR"/gradlew --no-daemon connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=small; fi
+"$PROJECT_DIR"/gradlew clean
+"$PROJECT_DIR"/gradlew build -Dscan
+"$PROJECT_DIR"/gradlew test
+"$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=small; fi
 if [ "$TEST_SUITE" == "integration" ]
 then
     if [ "$TRAVIS_BRANCH" == "development" ] || [ "$TRAVIS_BRANCH" == "master" ]
-    then "$PROJECT_DIR"/gradlew --no-daemon connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=medium
+    then "$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=medium
     fi
     if [ "$TRAVIS_BRANCH" == "master" ]
-    then "$PROJECT_DIR"/gradlew --no-daemon connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=large
+    then "$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=large
     fi
 fi
