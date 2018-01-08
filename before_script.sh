a93affc865329191abc5c9fd1d5a03592f15465e
mplazaspalacio <manuelplazaspalacio@gmail.com>
2018-01-08 14:14:25 +0100

    Moving scripts outside scripts folder.

--- before_script.sh
+++ before_script.sh
@@ -2,12 +2,12 @@
 
 if [ "$TEST_SUITE" == "integration" ]
 then
-./scripts/travis_create_avd.sh &
+./travis_create_avd.sh &
 fi
 if [ "$TEST_SUITE" == "integration_large" ]
 then
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] &&  [ "$TRAVIS_BRANCH" == "development" ] || [ "$TRAVIS_BRANCH" == "master" ]
     then
-    ./scripts/travis_create_avd.sh &
+    ./travis_create_avd.sh &
     fi
 fi
