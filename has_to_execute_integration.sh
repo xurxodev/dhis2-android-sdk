3f424ae40c9a73896a0117ca32f92c7fa8c083fc
mplazaspalacio <manuelplazaspalacio@gmail.com>
2018-02-01 17:47:53 +0100

    Saving in variable if it has to execute the integration test, an executing integration in all PR o in merge to development.

--- has_to_execute_integration.sh
+++ has_to_execute_integration.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-EXECUTE_INTEGRATION="false"
 if [ "$TEST_SUITE" == "integration" ] || [ "$TEST_SUITE" == "integration_large" ]
 then
     if [ "$TRAVIS_BRANCH" == "development" ] || [ "$TRAVIS_PULL_REQUEST" == "true" ]
     then
-    EXECUTE_INTEGRATION="true"
+    "$EXECUTE_INTEGRATION"="true"
     fi
 fi
-export EXECUTE_INTEGRATION
