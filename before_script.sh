9bbd550153be1b0c6146b7c31a66e22894fc6709
mplazaspalacio <manuelplazaspalacio@gmail.com>
2018-02-02 11:06:05 +0100

    saving in sh if has to execute integration tests

--- before_script.sh
+++ before_script.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$TEST_SUITE" == "integration" ] || [ "$TEST_SUITE" == "integration_large" ]
+if [ "$EXECUTE_INTEGRATION" == "true" ]
 then
-    if [ "$TRAVIS_BRANCH" == "development" ] || [ "$TRAVIS_PULL_REQUEST" == "true" ]
-    then
 ./travis_create_avd.sh &
-fi
 fi
--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -xe
+. has_to_execute_integration.sh
 
 # You can run it from any directory.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -10,22 +11,21 @@ if [ "$TEST_SUITE" == "units" ];
 then
 "$PROJECT_DIR"/gradlew clean
 "$PROJECT_DIR"/gradlew build -Dscan; fi
-if [ "$TEST_SUITE" == "integration" ] || [ "$TEST_SUITE" == "integration_large" ]
+
+if [ "$EXECUTE_INTEGRATION" == "true" ]
 then
-    if [ "$TRAVIS_BRANCH" == "development" ] || [ "$TRAVIS_PULL_REQUEST" == "true" ]
+android-wait-for-emulator
+sleep 180
+adb devices
+adb shell input keyevent 82 &
+    if [ "$TEST_SUITE" == "integration" ]
     then
-    android-wait-for-emulator
-    sleep 180
-    adb devices
-    adb shell input keyevent 82 &
-        if [ "$TEST_SUITE" == "integration" ]
-        then
-        "$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=small
-        "$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=medium
-        fi
-        if [ "$TEST_SUITE" == "integration_large" ]
-        then
-        "$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=large
-        fi
+    "$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=small
+    "$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=medium
+    fi
+    if [ "$TEST_SUITE" == "integration_large" ]
+    then
+    "$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=large
     fi
 fi
+
