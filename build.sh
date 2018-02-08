40876a37215900e54b2d8c4b962dd27563354e08
idelcano <ignaciodelcano@gmail.com>
2018-02-08 21:22:29 +0100

    try with 18 test commented 2

--- build.sh
+++ build.sh
@@ -18,13 +18,13 @@ android-wait-for-emulator
 sleep 180
 adb devices
 adb shell input keyevent 82 &
-    if [ "$TEST_SUITE" == "integration" ]
-    then
-    "$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=small
-    fi
-    if [ "$TEST_SUITE" == "integration_large" ]
-    then
-    "$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=medium
-    fi
+if [ "$TEST_SUITE" == "integration" ]
+then
+"$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=small
+fi
+if [ "$TEST_SUITE" == "integration_large" ]
+then
+"$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=medium
+fi
 fi
 
