a8bfa5dfbbb11843818027404a76a4a8a97463a7
idelcano <ignaciodelcano@gmail.com>
2018-02-09 23:17:06 +0100

    test with last good config

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -xe
-. has_to_execute_integration.sh
 
 # You can run it from any directory.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -10,21 +9,15 @@ PROJECT_DIR=$DIR/
 if [ "$TEST_SUITE" == "units" ];
 then
 "$PROJECT_DIR"/gradlew clean
-"$PROJECT_DIR"/gradlew build -Dscan; fi
-
-if [ "$EXECUTE_INTEGRATION" == "true" ]
+"$PROJECT_DIR"/gradlew build -Dscan
+"$PROJECT_DIR"/gradlew test
+"$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=small; fi
+if [ "$TEST_SUITE" == "integration" ]
 then
-android-wait-for-emulator
-sleep 180
-adb devices
-adb shell input keyevent 82 &
-    if [ "$TEST_SUITE" == "integration" ]
-    then
-    "$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=small
-    fi
-    if [ "$TEST_SUITE" == "integration_large" ]
-    then
-    "$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=medium
+    if [ "$TRAVIS_BRANCH" == "development" ] || [ "$TRAVIS_BRANCH" == "master" ]
+    then "$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=medium
     fi
 fi
-
+if [ "$TEST_SUITE" == "integration_large" ] && [ "$TRAVIS_BRANCH" == "master" ]
+then "$PROJECT_DIR"/gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.size=large
+fi
